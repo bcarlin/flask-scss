1ce0d9898fc31f08bbf5765b3a687eaa8067a465
Bruno Carlin <self@aerdhyl.eu>
2012-10-07 16:23:27 +0200

    Raise a DeprecationWarning when using pre-Flask-0.8 import scheme

--- flaskext/flask_scss.py
+++ flaskext/flask_scss.py
@@ -1,1 +1,6 @@
 from .scss import Scss
+
+from warnings import warn
+warn(DeprecationWarning('Deprecated import method. '
+                        'Please use:\n  '
+                        'from flask.ext.scss import Scss'), stacklevel=2)
