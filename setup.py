bfaf87ba39f16485250e88a791793f3695b30f04
Bruno Carlin <bruno@bcarlin.net>
2015-07-10 12:23:51 +0200

    upgrade the packaging procedure

--- setup.py
+++ setup.py
@@ -2,9 +2,23 @@
 Flask-scss
 """
 from setuptools import setup
+import os
 
 with open('README.rst') as readme:
     LONG_DOC = readme.read()
+
+
+def make_data_files(root_path, base_target):
+    ret = []
+    tmp = []
+    for elem in os.listdir(root_path):
+        full_path = os.path.join(root_path, elem)
+        if os.path.isfile(full_path):
+            tmp.append(full_path)
+        else:
+            ret += make_data_files(full_path, os.path.join(base_target, elem))
+    ret.append((base_target, tmp))
+    return ret
 
 setup(
     name='Flask-Scss',
@@ -21,6 +35,9 @@ setup(
     install_requires=[
         'Flask', 'pyScss'
     ],
+    data_files=make_data_files('doc/.build/html', 'doc') + [
+        ('.', ['tox.ini', 'LICENSE.txt', 'README.rst', 'AUTHORS']),
+    ],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
@@ -29,6 +46,6 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-        'Topic :: Software Development :: Libraries :: Python Modules'
+        'Topic :: Software Development :: Libraries :: Python Modules',
     ]
 )
